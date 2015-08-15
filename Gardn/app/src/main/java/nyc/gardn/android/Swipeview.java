52deed2ea44452bd6686d3b97cb8d9c68e0272af
Nick Tinsley <00sturm@gmail.com>
2015-08-15 18:54:52 -0400

    change theme

--- Gardn/app/src/main/java/nyc/gardn/android/Swipeview.java
+++ Gardn/app/src/main/java/nyc/gardn/android/Swipeview.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Swipeview extends AppCompatActivity {
+public class SwipeView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
