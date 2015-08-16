33490de9903bcd69955ac4e33d9ea720112a0838
Nick Tinsley <00sturm@gmail.com>
2015-08-15 21:36:08 -0400

    add Firebase

--- Gardn/app/src/main/java/nyc/gardn/android/GardnApplication.java
+++ Gardn/app/src/main/java/nyc/gardn/android/GardnApplication.java
@@ -2,6 +2,8 @@ package nyc.gardn.android;
 
 import android.app.Application;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by nicktinsley on 8/15/15.
  */
@@ -10,6 +12,6 @@ public class GardnApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        Firebase.setAndroidContext(this);
     }
 }
