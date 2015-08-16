e160f216d1483f5e4db8bf1cc16d419c8c3233d7
Nick Tinsley <00sturm@gmail.com>
2015-08-15 22:17:44 -0400

    add create activity stub

---
+++ Gardn/app/src/main/java/nyc/gardn/android/ui/CreateInvestorActivity.java
@@ -0,0 +1,18 @@
+package nyc.gardn.android.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import nyc.gardn.android.R;
+
+public class CreateInvestorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_investor);
+    }
+
+}
