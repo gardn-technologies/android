905cf09eb266809d3030d30a14763dcfe816837b
Nick Tinsley <00sturm@gmail.com>
2015-08-15 21:35:09 -0400

    removed Parse

--- Gardn/app/src/main/java/nyc/gardn/android/GardnApplication.java
+++ Gardn/app/src/main/java/nyc/gardn/android/GardnApplication.java
@@ -2,8 +2,6 @@ package nyc.gardn.android;
 
 import android.app.Application;
 
-import com.parse.Parse;
-
 /**
  * Created by nicktinsley on 8/15/15.
  */
@@ -12,9 +10,6 @@ public class GardnApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Parse.enableLocalDatastore(this);
-
-        Parse.initialize(this, SecretConstants.APPLICATON_ID, SecretConstants.CLIENT_KEY);
 
     }
 }
